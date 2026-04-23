8e90cb2a48f9c8d38f44a3a22558b96fa85e3bb3
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-04-23 18:03:16 +0200

    .

--- src/pages/home/components/hero/HeroImgModal.tsx
+++ src/pages/home/components/hero/HeroImgModal.tsx
@@ -7,7 +7,7 @@ const HeroImgModal = ({ piece, onNavigate }: HeroImgModalProps) => {
   const { t } = useTranslation();
   return (
     <div
-      className="relative w-full md:max-w-[800px] h-[90vh] p-4 md:p-8 rounded-lg bg-bg overflow-auto no-scrollbar flex flex-col text-center justify-around "
+      className="relative w-full md:max-w-[800px] p-4 md:p-8 rounded-lg bg-bg overflow-auto no-scrollbar flex flex-col text-center justify-around "
       onClick={(e) => e.stopPropagation()}
     >
       {piece && (
@@ -19,7 +19,7 @@ const HeroImgModal = ({ piece, onNavigate }: HeroImgModalProps) => {
             className="block w-[50%] h-auto md:h-[500px] block rounded-xl object-cover mx-auto mb-4"
           />
           <p className="text-text-theme text-xs mb-4 md:text-sm  overflow-auto no-scrollbar">{t(piece?.text ?? '')}</p>
-          <NavLink to={piece?.link ?? ''} onClick={onNavigate} className="btn text-center w-full">
+          <NavLink to={piece?.link ?? ''} onClick={onNavigate} className="btn text-center">
             {t('btn.heroImgModal')}
           </NavLink>
         </>
