053f60116c2390586cfb1a8ed621e699ae78e43f
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-04-23 17:33:07 +0200

    .

--- src/pages/home/components/hero/HeroInfo.tsx
+++ src/pages/home/components/hero/HeroInfo.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next';
 import nameDesk from '@/img/hero/nameDesk.webp';
 import nameMob from '@/img/hero/nameMob.webp';
 import dekor from '@/img/dekor/decorLotosHero.webp';
-import logoDesk from '@/img/hero/tulip_transparent_bg.png';
-import logoMob from '@/img/hero/tulip_transparent_bg.png';
+import logoDesk from '@/img/hero/tulip_transparent_clean.png';
+import logoMob from '@/img/hero/tulip_transparent_clean.png';
 
 const HeroInfo = () => {
   const { t } = useTranslation();
@@ -33,7 +33,7 @@ const HeroInfo = () => {
           height={80}
         />
       </picture>
-      <p className="text-accent font-bold text-lg md:text">{t('hero.slogan')}</p>
+      <p className="text-accent font-bold text-base md:text">{t('hero.slogan')}</p>
       <p className="text-center text-text-theme font-semibold text-sm mb-[16px] md:text-base md:mb-[24px]">
         {t('hero.description')}
       </p>
