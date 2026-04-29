ea89c39dcc09d1d488644e4405e10b50468b234c
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-04-29 16:47:25 +0200

    .

--- src/loyouts/MainLoyout.tsx
+++ src/loyouts/MainLoyout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import { Container, Header } from '@/shared';
 
+import superDecor from '@/img/dekor/ьтльдльольодльодл.png';
+
 const MainLoyout = () => {
   return (
     <Container>
@@ -11,6 +13,9 @@ const MainLoyout = () => {
       </main>
 
       <footer>Footer</footer>
+      <div className="pointer-events-none absolute left-0 top-0 z-10 lg:block">
+        <img src={superDecor} alt="" className="h-full  opacity-20" />
+      </div>
     </Container>
   );
 };
