3d41dcb5131ef973c67dc90afefa0e13d809a13e
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-04-29 16:28:04 +0200

    .

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,17 @@
 import { Hero } from './components';
+import superDecor from '@/img/dekor/ьтльдльольодльодл.png';
 
 export default function Home() {
   return (
-    <>
+    <div
+      style={{ '--decor-bg': `url(${superDecor})` } as React.CSSProperties}
+      className="before:content-[''] before:absolute before:inset-0
+  before:bg-[image:var(--decor-bg)]
+  before:bg-no-repeat before:bg-center before:bg-cover
+  before:opacity-30 before:pointer-events-none
+    "
+    >
       <Hero />
-    </>
+    </div>
   );
 }
