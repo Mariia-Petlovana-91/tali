00c5419799513c52632d680122775b082fccd9d3
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2026-05-02 00:38:34 +0200

    edit logo for liteHaus

--- src/shared/ui/Logo.tsx
+++ src/shared/ui/Logo.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router-dom';
 import logo from '@/img/hero/nameMob.webp';
 
 const Logo = () => {
   return (
-    <a href="/" aria-label="Go Home">
-      <img src={logo} alt="Talia K2O" width={560} height={130} className="w-[120px] md:w-[180px] h-auto" />
-    </a>
+    <Link to="/" aria-label="Go Home">
+      <img
+        src={logo}
+        alt="Talia K2O"
+        width={560}
+        height={130}
+        fetchPriority="high"
+        className="w-[120px] md:w-[180px] h-auto"
+      />
+    </Link>
   );
 };
 
